0b357353af51e7ab0da0032eaa158929013b30a6
kamal <detoxifiedplant@gmail.com>
2024-09-19 14:25:38 +0530

    phase 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #![allow(unused_imports)]
 
 use core::panic;
+use std::io::Cursor;
+use hashbrown::hash_map::Keys;
 use rand::distributions::{Uniform, Distribution};
 use std::borrow::Borrow;
 use std::cmp::max;
@@ -9,6 +11,46 @@ use std::collections::HashMap as StdHashMap;
 use std::fmt::{Debug, Display};
 use std::hash::{Hash, Hasher};
 use std::mem::{swap, take};
+pub mod raw;
+
+const EMPTY: u8 = 0b1111_1111;
+const DELETED: u8 = 0b1000_0000;
+const GROUP_SIZE: u8 = 16;
+type GroupWord = u64;
+pub(crate) struct Group(GroupWord);
+
+impl Group{
+    pub(crate) unsafe fn load(ptr: *const u8) -> Self {
+        Group(ptr::read_unaligned(ptr.cast()))
+    }
+}
+
+// Checks whether a control byte represents a full bucket (top bit is clear).
+#[inline]
+fn is_full(ctrl: u8) -> bool {
+    ctrl & 0x80 == 0
+}
+
+// Checks whether a control byte represents a special value (top bit is set).
+#[inline]
+fn is_special(ctrl: u8) -> bool {
+    ctrl & 0x80 != 0
+}
+
+// Checks whether a special control value is EMPTY (just check 1 bit).
+#[inline]
+fn special_is_empty(ctrl: u8) -> bool {
+    debug_assert!(is_special(ctrl));
+    ctrl & 0x01 != 0
+}
+
+const BIT_SHIFT: u8 = 64 - 7;
+fn h2(hash: u64) -> u8 {
+    let top7_bits = hash >> (BIT_SHIFT);
+    (top7_bits & 0x7f) as u8 // truncation
+}
+
+
 
 #[derive(Debug)]
 enum Entry<Key, Value> {
@@ -20,6 +62,11 @@ enum Entry<Key, Value> {
 #[derive(Debug)]
 struct HashMap<Key, Val> {
     data: Vec<Entry<Key, Val>>,
+
+    // Mask to get an index from a hash value. The value is one less than the
+    // number of buckets in the table.
+    control_bytes: Vec<u8>,
+
     n_occupied: usize,
     n_vacant: usize,
 }
@@ -52,55 +99,57 @@ impl<Key, Val> Entry<Key, Val> {
     }
 }
 
-pub trait MapTrait<Key, Val> {
-    fn new() -> Self;
-    fn insert(&mut self, key: Key, val: Val) -> Option<Val>;
-    fn get(&self, key: &Key) -> Option<&Val>;
-    fn remove(&mut self, key: &Key) -> Option<Val>;
-    fn get_mut(&mut self, key: &Key) -> Option<&mut Val>;
-    fn len(&self) -> usize;
-    fn is_empty(&self) -> bool;
-}
+impl<Key: Eq + Hash + Debug, Val> HashMap<Key, Val> {
+    // unsafe fn find_inner(&self, hash: u64, eq: &mut dyn FnMut(usize) -> bool) -> Option<usize> {
+    //     let h2_hash = h2(hash);
+    //     let mut probe_seq = self.probe_seq(hash);
+    //
+    //     loop {
+    //         // SAFETY:
+    //         // * Caller of this function ensures that the control bytes are properly initialized.
+    //         //
+    //         // * `ProbeSeq.pos` cannot be greater than `self.bucket_mask = self.buckets() - 1`
+    //         //   of the table due to masking with `self.bucket_mask`.
+    //         //
+    //         // * Even if `ProbeSeq.pos` returns `position == self.bucket_mask`, it is safe to
+    //         //   call `Group::load` due to the extended control bytes range, which is
+    //         //  `self.bucket_mask + 1 + Group::WIDTH` (in fact, this means that the last control
+    //         //   byte will never be read for the allocated table);
+    //         //
+    //         // * Also, even if `RawTableInner` is not already allocated, `ProbeSeq.pos` will
+    //         //   always return "0" (zero), so Group::load will read unaligned `Group::static_empty()`
+    //         //   bytes, which is safe (see RawTableInner::new_in).
+    //         let group = unsafe { Group::load(self.ctrl(probe_seq.pos)) };
+    //
+    //         for bit in group.match_byte(h2_hash) {
+    //             // This is the same as `(probe_seq.pos + bit) % self.buckets()` because the number
+    //             // of buckets is a power of two, and `self.bucket_mask = self.buckets() - 1`.
+    //             let index = (probe_seq.pos + bit) & self.bucket_mask;
+    //
+    //             if likely(eq(index)) {
+    //                 return Some(index);
+    //             }
+    //         }
+    //
+    //         if likely(group.match_empty().any_bit_set()) {
+    //             return None;
+    //         }
+    //
+    //         probe_seq.move_next(self.bucket_mask);
+    //     }
+    // }
 
-// impl<Key, Val> MapTrait<Key, Val> for HashMap<Key, Val> {}
-impl<Key, Val> MapTrait<Key, Val> for std::collections::HashMap<Key, Val>
-where
-    Key: std::cmp::Eq + std::hash::Hash,
-{
-    fn new() -> Self {
-        StdHashMap::new()
-    }
-    fn insert(&mut self, key: Key, val: Val) -> Option<Val> {
-        StdHashMap::insert(self, key, val)
-    }
-    fn get(&self, key: &Key) -> Option<&Val> {
-        StdHashMap::get(self, key)
-    }
-    fn remove(&mut self, key: &Key) -> Option<Val> {
-        StdHashMap::remove(self, key)
-    }
-    fn get_mut(&mut self, key: &Key) -> Option<&mut Val> {
-        StdHashMap::get_mut(self, key)
-    }
-    fn len(&self) -> usize {
-        StdHashMap::len(self)
-    }
-    fn is_empty(&self) -> bool {
-        self.is_empty()
-    }
-}
-
-impl<Key: Eq + Hash + Debug, Val> MapTrait<Key, Val> for HashMap<Key, Val> {
     fn new() -> Self {
         Self {
-            data: Vec::new(),
+            data: Vec::with_capacity(64),
+            control_bytes: vec![EMPTY; 64],
             n_occupied: 0,
             n_vacant: 0,
         }
     }
 
     fn insert(&mut self, key: Key, value: Val) -> Option<Val> {
-        if self.load_factor() >= 0.75 {
+        if self.load_factor() >= 0.85 {
             self.resize();
         }
 
@@ -181,27 +230,35 @@ impl<Key: Eq + Hash + Debug, Val> MapTrait<Key, Val> for HashMap<Key, Val> {
     fn is_empty(&self) -> bool {
         self.n_occupied == 0
     }
-}
-
-impl<Key: Eq + Hash + Debug, Val> HashMap<Key, Val> {
-    fn index<Q>(&self, hash: usize, _k: &Q) -> usize
-    where
-        Key: Borrow<Q>,
-        Q: Eq + Hash + Debug,
-    {
-        println!("{_k:?} {:?}", hash);
-        hash & (self.data.len() - 1)
-    }
-
-    fn get_index<Q>(&self, key: &Q) -> usize
+    fn get_index<Q>(&self, key: &Q) -> u64
     where
         Key: Borrow<Q>,
         Q: Eq + Hash + Debug,
     {
         let mut hasher = DefaultHasher::new();
         key.hash(&mut hasher);
-        // hasher.finish() as usize % self.data.len()
-        self.index(hasher.finish() as usize, key)
+        let hash = hasher.finish();
+
+        let h2_hash = h2(hash);
+        let number_of_groups = self.data.capacity() >> 4;
+        let mut target_group = hash & (number_of_groups as u64 - 1);
+        for i in 0..number_of_groups {
+            for bit in target_group.match_byte(h2_hash) {
+                let index = (probe_seq.pos + bit) & self.bucket_mask;
+
+                if likely(eq(index)) {
+                    return Some(index);
+                }
+            }
+
+            if likely(group.match_empty().any_bit_set()) {
+                return None;
+            }
+            // We should have found an empty bucket by now and ended the probe.
+            target_group = target_group + ((i as u64 + 1) * i as u64) / 2;
+        }
+
+        0
     }
 
     fn iter_mut_starting_at(&mut self, idx: usize) -> impl Iterator<Item = &mut Entry<Key, Val>> {
@@ -211,9 +268,9 @@ impl<Key: Eq + Hash + Debug, Val> HashMap<Key, Val> {
 
     fn load_factor(&self) -> f64 {
         if self.data.is_empty() {
-            1.0
+            0.0
         } else {
-            1.0 - self.n_vacant as f64 / self.data.len() as f64
+            self.n_occupied as f64 / self.data.capacity() as f64
         }
     }
 
@@ -231,6 +288,7 @@ impl<Key: Eq + Hash + Debug, Val> HashMap<Key, Val> {
 
         let mut new_table = Self {
             data: (0..new_size).map(|_| Entry::Vacant).collect(),
+            control_bytes: vec![EMPTY; new_size],
             n_occupied: 0,
             n_vacant: new_size,
         };
@@ -285,11 +343,8 @@ mod tests {
         // test::<std::collections::HashMap<i64, i64>>();
     }
 
-    fn test<M>()
-    where
-        M: MapTrait<i64, i64> + Debug,
-    {
-        let mut map = M::new();
+    fn test() {
+        let mut map = HashMap::new();
 
         let key_gen = Uniform::from(0..1000);
         let op_gen = Uniform::from(0..4);
