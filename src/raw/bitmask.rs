3510df9cdaee50c0c95632bbbbccbb7c985fc199
kamal <detoxifiedplant@gmail.com>
2024-09-22 17:54:07 +0530

    comments added for bit manipulation and sse instructions

--- src/raw/bitmask.rs
+++ src/raw/bitmask.rs
@@ -1,7 +1,6 @@
 use core::num::NonZeroU16;
 pub(crate) type BitMaskWord = u16;
 pub(crate) type NonZeroBitMaskWord = NonZeroU16;
-pub(crate) const BITMASK_STRIDE: usize = 1;
 pub(crate) const BITMASK_ITER_MASK: BitMaskWord = !0;
 
 #[derive(Copy, Clone)]
@@ -9,20 +8,30 @@ pub(crate) struct BitMask(pub(crate) BitMaskWord);
 
 #[allow(clippy::use_self)]
 impl BitMask {
+    // removes last bit set to 1
+    //   0001_1000
+    // & 0001_0111 (value - 1)
+    //   0001_0000
     fn remove_lowest_bit(self) -> Self {
         BitMask(self.0 & (self.0 - 1))
     }
 
+    // comparing `BitMaskWord` with 0000_0000
+    // returns true if any bit set to 1
     pub(crate) fn any_bit_set(self) -> bool {
         self.0 != 0
     }
 
+    // returns index of lowest bit set from end and None if all bits are 0
+    // 0000_1000 -> Some(3)
+    // 0010_0000 -> Some(5)
+    // 0000_0000 -> None
     pub(crate) fn lowest_set_bit(self) -> Option<usize> {
         NonZeroBitMaskWord::new(self.0).map(Self::nonzero_trailing_zeros)
     }
 
     fn nonzero_trailing_zeros(nonzero: NonZeroBitMaskWord) -> usize {
-        nonzero.trailing_zeros() as usize / BITMASK_STRIDE
+        nonzero.trailing_zeros() as usize
     }
 }
 
@@ -30,6 +39,8 @@ impl IntoIterator for BitMask {
     type Item = usize;
     type IntoIter = BitMaskIter;
 
+    // returns index of bits set to 1 form end
+    // 0001_0010 -> 1, 4
     fn into_iter(self) -> BitMaskIter {
         // A BitMask only requires each element (group of bits) to be non-zero.
         // However for iteration we need each element to only contain 1 bit.
--- src/raw/sse2.rs
+++ src/raw/sse2.rs
@@ -1,5 +1,5 @@
-use super::EMPTY;
 use core::mem;
+use super::EMPTY;
 use super::bitmask::BitMask;
 
 #[cfg(target_arch = "x86_64")]
@@ -10,19 +10,31 @@ pub(crate) struct Group(x86::__m128i);
 
 impl Group {
 
+    // loads 128-bit data from given pointer
     pub(crate) unsafe fn load(ptr: *const u8) -> Self {
         Group(x86::_mm_loadu_si128(ptr.cast()))
     }
 
     pub(crate) fn match_byte(self, byte: u8) -> BitMask {
         unsafe {
+            // _mm_set1_epi8 creates a vector for given value
+            // _mm_set1_epi8(42) -> [42; 16]
+            // _mm_cmpeq_epi8 conpares both vector
+            // returns high bit values for each matching data
+            // [42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42]
+            // [41, 40, 39, 42, 44, 45, 44, 42, 47, 52, 56, 64, 67, 80, 22, 26]
+            // [ 0,  0,  0, FF,  0,  0,  0, FF,  0,  0,  0,  0,  0,  0,  0,  0]
             let cmp = x86::_mm_cmpeq_epi8(self.0, x86::_mm_set1_epi8(byte as i8));
+            // sets 1 if most significant bit is set and returns i32 of it
+            // [ 0,  0,  0, FF,  0,  0,  0, FF,  0,  0,  0,  0,  0,  0,  0,  0]
+            // 0001_0001_0000_0000 integer value
             let res = x86::_mm_movemask_epi8(cmp) as u16;
             BitMask(res)
         }
     }
 
     pub(crate) fn match_empty(self) -> BitMask {
+        // compares loaded data with EMPTY bytes
         self.match_byte(EMPTY)
     }
 
